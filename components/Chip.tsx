25529b4c7a622f9c69eeee72cee049d9c54d3ba0
hxutixnnn <ng.huutien98@gmail.com>
2023-03-02 12:50:07 +0700

    fix: tailwind classnames not override

--- components/Chip.tsx
+++ components/Chip.tsx
@@ -1,11 +1,11 @@
-import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
 import { HTMLAttributes } from "react";
 
 function Chip({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
   return (
     <div
       {...props}
-      className={clsx(
+      className={twMerge(
         "border rounded-2xl py-1 px-4 text-slate-500 text-xl mb-5 hover:scale-105 transition duration-300 ease-in-out",
         className
       )}
